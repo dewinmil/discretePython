61019b50dcefb42329977966ff3aaa81b537b826
Miles De Wind <miles.dewind@mail.gvsu.edu>
2017-12-07 13:26:47 -0500

    coloring

--- coloring.py
+++ coloring.py
@@ -1,5 +1,4 @@
 def is_proper(graph, color):
-  adjacent= [];
   for n in graph:
     for c in graph[n]:
       if color[n] == color[c]:
@@ -7,3 +6,31 @@ def is_proper(graph, color):
   return True
 
 
+
+def three_color(graph):
+  allTrees = []
+  currentTree =[]
+  num = 1
+  count = 0
+  currentPos = 0
+
+  for n in graph:
+    if maxCount == 0:
+      count++
+    n[1] = num
+    currentTree.append(n)
+
+  allTrees.append(currentTree)
+  currentTree = []
+  maxCount = count
+
+  return allTrees
+  
+
+
+def is_proper_edge(graph):
+  
+
+
+def greedy(graph, order):
+   
